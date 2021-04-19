eb0fc7102687e673cf628734ebd668ad0d51c769
rama-ctrl <hectoradventures@gmail.com>
2021-04-20 01:45:14 +0530

    c2

--- js/app.js
+++ js/app.js
@@ -15,4 +15,40 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+  //submit budget method
+
+  submitBudgetForm(){
+    const value = this.budgetInput.value;
+    if (value === '' || value<0){
+      this.budgetFeedback.classList.add("showItem");
+      this.budgetFeedback.innerHTML = '<p> Value cannot be empty or negative </p>';
+    }
+  }
+
 }
+function eventListeners(){
+  const budgetForm = document.getElementById('budget-form');
+  const expenseForm = document.getElementById('expense-form');
+  const expenseList = document.getElementById('expense-list');
+
+  //new instance of UI class
+
+  const ui = new UI();
+
+  //budget from submit
+budgetForm.addEventListener('submit',function(event){
+  event.preventDefault();
+})
+expenseForm.addEventListener('submit',function(event){
+  event.preventDefault();
+})
+expenseList.addEventListener('click',function(event){
+
+})
+}
+document.addEventListener('DOMContentLoaded',function(){
+  eventListeners();
+})
+
+document.write('Hello Test.');
+console.log('Hello World');
